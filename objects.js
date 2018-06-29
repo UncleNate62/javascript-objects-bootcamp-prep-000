2ddf2b1ebe2612b7aa80edcf61f89ca6df6fbedc
Nathan T Case <unclenate62@gmail.com>
2018-06-29 16:47:02 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,11 @@
 var playlist = { ladyGaga: "Bad Romance" };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-  return playlist
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName];
+  return playlist;
 }
